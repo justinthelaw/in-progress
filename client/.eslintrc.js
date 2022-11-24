5bb9747823a658f88e5465a6de4c781e8c5a6e42
justinthelaw <justinwingchunglaw@gmail.com>
2022-11-24 15:43:35 -0500

    typescript react app initialized

---
+++ client/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Justin Law
+      </header>
+    </div>
+  );
+}
+
+export default App;
