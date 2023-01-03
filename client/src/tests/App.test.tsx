2eff862df30d51c72612b5ed9541a509162ccd2e
justinthelaw <justinwingchunglaw@gmail.com>
2023-01-03 07:40:15 -0800

    React TS redo clean-up

--- client/src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('Renders Justin Law', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Justin Law/i);
-  expect(linkElement).toBeInTheDocument();
-});
